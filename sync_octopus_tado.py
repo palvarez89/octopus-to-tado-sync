2ccb9a4b62ac32289759fda3747603b2bcff3cc4
Pedro Alvarez <pedro@alvarezpiedehierro.com>
2025-04-13 20:02:56 +0100

    Login using browser

--- sync_octopus_tado.py
+++ sync_octopus_tado.py
@@ -1,7 +1,9 @@
 import argparse
+import asyncio
 import requests
 from datetime import datetime
 from requests.auth import HTTPBasicAuth
+from playwright.async_api import async_playwright
 from PyTado.interface import Tado
 
 
@@ -14,9 +16,7 @@ def get_meter_reading_total_consumption(api_key, mprn, gas_serial_number):
     total_consumption = 0.0
 
     while url:
-        response = requests.get(
-            url, auth=HTTPBasicAuth(api_key, "")
-        )
+        response = requests.get(url, auth=HTTPBasicAuth(api_key, ""))
 
         if response.status_code == 200:
             meter_readings = response.json()
@@ -34,11 +34,71 @@ def get_meter_reading_total_consumption(api_key, mprn, gas_serial_number):
     return total_consumption
 
 
+async def browser_login(url, username, password):
+
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(
+            headless=True
+        )  # Set to True if you don't want a browser window
+        context = await browser.new_context()
+        page = await context.new_page()
+
+        await page.goto(url)
+
+        # Click the "Submit" button before login
+        await page.wait_for_selector('text="Submit"', timeout=5000)
+        await page.click('text="Submit"')
+
+        # Wait for the login form to appear
+        await page.wait_for_selector('input[name="loginId"]')
+
+        # Replace with actual selectors for your site
+        await page.fill('input[id="loginId"]', username)
+        await page.fill('input[name="password"]', password)
+
+        await page.click('button.c-btn--primary:has-text("Sign in")')
+
+        # Optionally take a screenshot
+        await page.screenshot(path="screenshot.png")
+
+        await page.wait_for_selector(
+            ".text-center.message-screen.b-bubble-screen__spaced", timeout=10000
+        )
+
+        # Take a screenshot (optional)
+        await page.screenshot(path="after-message.png")
+        await browser.close()
+
+
+def tado_login(username, password):
+    tado = Tado(token_file_path="/tmp/tado_refresh_token")
+
+    status = tado.device_activation_status()
+
+    if status == "PENDING":
+        url = tado.device_verification_url()
+
+        asyncio.run(browser_login(url, username, password))
+
+        tado.device_activation()
+
+        status = tado.device_activation_status()
+
+    if status == "COMPLETED":
+        print("Login successful")
+    else:
+        print(f"Login status is {status}")
+
+    return tado
+
+
 def send_reading_to_tado(username, password, reading):
     """
     Sends the total consumption reading to Tado using its Energy IQ feature.
     """
-    tado = Tado(username, password)
+
+    tado = tado_login(username=username, password=password)
+
     result = tado.set_eiq_meter_readings(reading=int(reading))
     print(result)
 
